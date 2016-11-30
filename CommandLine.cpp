5d3a70110eeb61051ea133ab896ea026d6a0c900
erumbold <erumbold@ithaca.edu>
2016-11-30 13:29:33 -0500

    completed help()

--- CommandLine.cpp
+++ CommandLine.cpp
@@ -39,6 +39,25 @@ static void CommandLine::run() {
     }
 }
 
+static void CommandLine::help() {
+    std::cout << "I <title> 'Inquire': Displays all information for a specific title" << std::endl;
+    std::cout << "L 'List': Lists the information for the entire inventory alphabetically" << std::endl;
+    std::cout << "A <title> 'Add': Adds a movie to the inventory. Prompt for initial 'want' and 'have' value. If the "
+            "title already exists, informs the user and print the information for that title." << std::endl;
+    std::cout << "M <title> 'Modify': Modifies the 'want' value for the specified title. Displays current 'want' and "
+            "'have' values, and prompts user for new 'want' value" << std::endl;
+    std::cout << "S <title> 'Sell': Decreases the count for the specified title by 1. If the title doesn't exist yet, "
+            "it is added. If the title is sold out or doesn't exist, prompts the user for the buyer's name and enters "
+            "them on the wait list for that title." << std::endl;
+    std::cout << "O <filename> 'Order': Creates a bulk purchase order for additional movies" << std::endl;
+    std::cout << "D <filename> 'Delivery': Reads file listing the delivery shipment of movies and updates the 'have' "
+            "values in the inventory. If a title doesn't exist in the inventory, it is added. "
+            "If there is a wait list for a movie, the name of the person will be printed and the movie will not be "
+            "counted in the inventory." << std::endl;
+    std::cout << "R <filename> 'Return': Writes a return invoice to a file specifying all books to be returned." << std::endl;
+    std::cout << "Q 'Quit': Saves the inventory and wait lists in a file and terminates execution" <<std::endl;
+}
+
 static void CommandLine::inquire(std::string title) {
     Movie* movie = stock->findMovie(title);
     if (movie == nullptr) {
