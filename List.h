503a9b4df54548b0955c62bb88f92e4e90cfbda5
erumbold <erumbold@ithaca.edu>
2016-12-03 22:33:42 -0500

    parent classes

---
+++ List.h
@@ -0,0 +1,31 @@
+//
+// Created by Erika Rumbold on 12/3/16.
+//
+
+#ifndef INC_220TERMPROJECT_LIST_H
+#define INC_220TERMPROJECT_LIST_H
+
+#include <string>
+
+template <class ItemType>
+class List {
+
+public:
+    virtual ~List(){}
+    virtual void addToEnd(ItemType& itemToAdd)=0;
+    virtual void addToFront(ItemType& itemToAdd)=0;
+    virtual void add(ItemType& itemToAdd, int index)=0;
+    virtual ItemType& get(int index)=0;
+    virtual ItemType remove(int index)=0;
+    virtual bool isEmpty()=0;
+    virtual int size()=0;
+    virtual void clearList()=0;
+    virtual int find(ItemType& itemToFind)=0;
+    virtual int findLast(ItemType& itemToFind)=0;
+    virtual std::string toString()=0;
+    virtual long getTotalLinesRun()=0;
+    virtual void resetTotalLinesRun()=0;
+    virtual int calcSizeOf()=0;
+};
+
+#endif //INC_220TERMPROJECT_LIST_H
---
+++ Queue.h
@@ -0,0 +1,18 @@
+//
+// Created by Erika Rumbold on 12/3/16.
+//
+
+#ifndef INC_220TERMPROJECT_QUEUE_H
+#define INC_220TERMPROJECT_QUEUE_H
+
+template <class ItemType>
+class Queue{
+
+public:
+    virtual ~Queue(){}
+    virtual void enqueue(ItemType item)=0;
+    virtual ItemType dequeue()=0;
+    virtual bool isEmpty()=0;
+};
+
+#endif //INC_220TERMPROJECT_QUEUE_H
